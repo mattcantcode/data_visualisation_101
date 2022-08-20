12df1eed42377dadf174c23cfd2be97bea92065e
mattcantcode <matt@everybodydancenow.net>
2022-08-20 14:04:33 +0100

    Plot high temperatures
    - Imported pyplot from matplotlib.
    - Plotted high temperatures against days in July 2018.

--- sitka_highs.py
+++ sitka_highs.py
@@ -1,5 +1,7 @@
 import csv
 
+import matplotlib.pyplot as plt
+
 filename = 'data/sitka_weather_07-2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
@@ -11,4 +13,15 @@ with open(filename) as f:
         high = int(row[5])
         highs.append(high)
 
-print (highs)
+# Plot the high temperatures.
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(highs, c='red')
+
+# Format plot.
+ax.set_title("Daily high temperatures, July 2018", fontsize=24)
+ax.set_xlabel('', fontsize=16)
+ax.set_ylabel("Temperature (F)", fontsize=16)
+ax.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
