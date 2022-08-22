212454f2fbe09e3820d615eeeaa99acee6a7c14e
mattcantcode <matt@everybodydancenow.net>
2022-08-22 11:03:08 +0100

    Update eq_explore_data.py
    - Deleted line for printing the quantity of earthquakes.
    - Added block that produces a list of the magnitudes of each earthquake, then prints a sub-list containing the first ten items.

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -6,7 +6,13 @@ with open(filename) as f:
     all_eq_data = json.load(f)
 
 all_eq_dicts = all_eq_data['features']
-print(len(all_eq_dicts))
+
+mags = []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    mags.append(mag)
+
+print(mags[:10])
 
 # readable_file = 'data/readable_eq_data.json'
 # with open(readable_file, 'w') as f:
